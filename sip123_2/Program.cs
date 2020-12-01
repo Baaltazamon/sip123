c269d264f9e17acea2b1659464c2084ea7d360e1
Илья Палатов <3gpabbiu4eji@mail.ru>
2020-12-01 18:03:01 +0300

    goodДобавьте файлы проекта.

---
+++ sip123_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sip123_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = GenerateString();
+            Console.WriteLine(result);
+            string answer = Console.ReadLine();
+            if (result == answer)
+                Console.WriteLine("Все верно");
+            else
+                Console.WriteLine("Вы ошиблись");
+            Console.ReadKey();
+        }
+        static public string GenerateString()
+        {
+            Random rnd = new Random();
+            string pool = "qwertyuiop[]asdfghjkl;'zxcvbnm,.";
+            string c = "";
+            for (int i=0;i<10;i++)
+            {
+                c+=pool[rnd.Next(pool.Length)];
+            }
+            return c;
+        }
+    }
+}
